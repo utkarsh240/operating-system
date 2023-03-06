160d19adabdd402bcf14b46a74000fb6bda1df01
utkarsh240 <93428291+utkarsh240@users.noreply.github.com>
2023-03-06 12:51:05 +0530

    Create 4.sh

---
+++ 6march/4.sh
@@ -0,0 +1,17 @@
+# input 2 number to find which one is biggest
+
+echo "Enter two numbers"
+
+read a b
+
+if [ $a -gt $b ]
+
+then
+
+echo "$a is greater than $b"
+
+else
+
+echo "$b is greater than $a"
+
+fi
