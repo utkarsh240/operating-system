ff74aad8eb083531f486c771e3e505c3e45bf6c4
utkarsh240 <93428291+utkarsh240@users.noreply.github.com>
2023-03-06 12:49:36 +0530

    Create 1.sh

---
+++ 6march/1.sh
@@ -0,0 +1,17 @@
+# check whether a number is positive or negative
+
+echo "Enter a number"
+
+read num
+
+if [ $num -gt 0 ]
+
+then
+
+echo "Number is positive"
+
+else
+
+echo "Number is negative"
+
+fi
