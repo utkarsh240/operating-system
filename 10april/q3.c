668dd1849d9399b37d7452bbae7b63972b8e4e1a
utkarsh240 <93428291+utkarsh240@users.noreply.github.com>
2023-04-10 13:02:36 +0530

    Create q3.c

---
+++ 10april/q3.c
@@ -0,0 +1,21 @@
+#include <stdio.h> 
+#include<pthread.h>
+int a = 3;
+void *f1()
+{
+    for (int i = 0; i < 10; i++){
+        a++;
+        printf("Hello World\n");
+    }
+}
+int main()
+{
+    pthread_t th1, th2;
+    pthread_create(&th1, NULL, f1, NULL);
+    pthread_create(&th2, NULL, f1, NULL);
+    pthread_join(th1, NULL);
+    pthread_join(th2, NULL);
+    printf("%d\n", a);
+    printf("main terminates\n");
+    return 0;
+}
